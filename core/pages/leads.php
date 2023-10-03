a23844b1e4db22a6f81cd90362d893e0a17f2590
Ashot Agabekov <info@leadee.io>
2023-10-03 18:53:26 +0300

    secure fix

--- core/pages/leads.php
+++ core/pages/leads.php
@@ -1,52 +1,53 @@
 <?php
-//leads template
-$scriptsLoader = new ScriptsLoader();
-$scriptsLoader->load_scripts_page_leads();
-$functions = new LeadeeFunctions();
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+// leads template
+$scripts_loader = new LEADEE_Scripts_Loader();
+$scripts_loader->load_scripts_page_leads();
+$functions = new LEADEE_Functions();
 ?>
 <section>
-    <div class="card" id="leads-stat">
-        <div class="card-content">
-            <div class="main-chart">
-                <h2 class="title-medium"><?php _e('Leads statistics',LEADEE_PLUGIN_NAME); ?></h2>
-                <div class="title-detail"><?php _e('Average count of leads',LEADEE_PLUGIN_NAME); ?></div>
-                <canvas id="main-chart" width="400" height="400" class=""></canvas>
-            </div>
-        </div>
-    </div>
-    
-    <div class="card table-leads" id="table-leads">
-        <div id="leads-filter-block"></div>
-        <div class="row">
-            <div class="col-100">
-                <table id="leads-list" class="table leads-table">
-                    <thead>
-                    <tr>
-                        <?php if ($functions->get_setting_option_value('leads-table-colums', 'dt') == 1) { ?>
-                            <th><?php _e('Date',LEADEE_PLUGIN_NAME); ?></th><?php } ?>
-                        <th><?php _e('Form fields',LEADEE_PLUGIN_NAME); ?></th>
-                        <th><?php _e('Source type',LEADEE_PLUGIN_NAME); ?></th>
-                        <th><?php _e('Device type',LEADEE_PLUGIN_NAME); ?></th>
-                        <th><?php _e('Page',LEADEE_PLUGIN_NAME); ?></th>
-                        <th><?php _e('Device OS',LEADEE_PLUGIN_NAME); ?></th>
-                        <th><?php _e('Form type',LEADEE_PLUGIN_NAME); ?></th>
-                        <th><?php _e('Form',LEADEE_PLUGIN_NAME); ?></th>
-                        <?php if ($functions->isEnableColumn('source')) { ?>
-                            <th><?php _e('Source',LEADEE_PLUGIN_NAME); ?></th><?php } ?>
-                        <?php if ($functions->isEnableColumn('first_url_parameters')) { ?>
-                            <th><?php _e('First visit params',LEADEE_PLUGIN_NAME); ?></th><?php } ?>
-                        <?php if ($functions->isEnableColumn('device_browser')) { ?>
-                            <th><?php _e('Browser',LEADEE_PLUGIN_NAME); ?></th><?php } ?>
-                        <?php if ($functions->isEnableColumn('device_screen_size')) { ?>
-                            <th><?php _e('Screen',LEADEE_PLUGIN_NAME); ?></th><?php } ?>
-                        <th><?php _e('Cost',LEADEE_PLUGIN_NAME); ?></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-    </div>
+	<div class="card" id="leads-stat">
+		<div class="card-content">
+			<div class="main-chart">
+				<h2 class="title-medium"><?php esc_html_e( 'Leads statistics', 'leadee' ); ?></h2>
+				<div class="title-detail"><?php esc_html_e( 'Average count of leads', 'leadee' ); ?></div>
+				<canvas id="main-chart" width="400" height="400" class=""></canvas>
+			</div>
+		</div>
+	</div>
+	<div class="card table-leads" id="table-leads">
+		<div id="leads-filter-block"></div>
+		<div class="row">
+			<div class="col-100">
+				<table id="leads-list" class="table leads-table">
+					<thead>
+					<tr>
+						<?php if ( $functions->get_setting_option_value( 'leads-table-columns', 'dt' ) === '1' ) { ?>
+							<th><?php esc_html_e( 'Date', 'leadee' ); ?></th><?php } ?>
+						<th><?php esc_html_e( 'Form fields', 'leadee' ); ?></th>
+						<th><?php esc_html_e( 'Source type', 'leadee' ); ?></th>
+						<th><?php esc_html_e( 'Device type', 'leadee' ); ?></th>
+						<th><?php esc_html_e( 'Page', 'leadee' ); ?></th>
+						<th><?php esc_html_e( 'Device OS', 'leadee' ); ?></th>
+						<th><?php esc_html_e( 'Form type', 'leadee' ); ?></th>
+						<th><?php esc_html_e( 'Form', 'leadee' ); ?></th>
+						<?php if ( $functions->is_enable_column( 'source' ) ) { ?>
+							<th><?php esc_html_e( 'Source', 'leadee' ); ?></th><?php } ?>
+						<?php if ( $functions->is_enable_column( 'first_url_parameters' ) ) { ?>
+							<th><?php esc_html_e( 'First visit params', 'leadee' ); ?></th><?php } ?>
+						<?php if ( $functions->is_enable_column( 'device_browser' ) ) { ?>
+							<th><?php esc_html_e( 'Browser', 'leadee' ); ?></th><?php } ?>
+						<?php if ( $functions->is_enable_column( 'device_screen_size' ) ) { ?>
+							<th><?php esc_html_e( 'Screen', 'leadee' ); ?></th><?php } ?>
+						<th><?php esc_html_e( 'Cost', 'leadee' ); ?></th>
+					</tr>
+					</thead>
+					<tbody>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </section>
--- uninstall.php
+++ uninstall.php
@@ -1,19 +1,16 @@
 <?php
-
-
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-function leadee_delete_plugin()
-{
-    global $wpdb;
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}leadee_base_default_social");
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}leadee_base_default_serp");
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}leadee_base_default_advert");
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}leadee_settings");
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}leadee_targets");
+function leadee_delete_plugin() {
+	global $wpdb;
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}leadee_base_default_social" );
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}leadee_base_default_serp" );
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}leadee_base_default_advert" );
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}leadee_settings" );
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}leadee_targets" );
 }
 
 leadee_delete_plugin();
